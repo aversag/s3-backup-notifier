0fec19074c0b4837aa2042254625494c29ecaccc
guillaume aversa <guillaume.aversa@gmail.com>
2020-05-05 11:19:47 +0200

    fix multiple buckets

--- README.md
+++ README.md
@@ -51,7 +51,6 @@ $ make deploy \
     PROJECT=<your_project_name> \
     ENV=<your_env> \
     MONITORING_BUCKETS=<buckets_to_monitor> \
-    S3_PREFIX=<s3_prefix> \
     SENDER=<sender_email> \
     RECIPIENTS='<recipient_email1> <recipient_email2>' \
     AWS_REGION='<your_aws_region>' \
--- python/handlers.py
+++ python/handlers.py
@@ -24,21 +24,22 @@ session = boto3.Session(region_name=aws_region)
 s3 = session.resource('s3')
 ses = session.client('ses', region_name=aws_ses_region)
 
-for bucket_name in bucket_names:
-    bucket = s3.Bucket(bucket_name)
-    objs = bucket.objects.filter(Prefix=s3_prefix).all()
-
-    # Convert to Human Readable
-    def sizeof_fmt(num, suffix='B'):
-        for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-            if abs(num) < 1024.0:
-                return "%3.1f%s%s" % (num, unit, suffix)
-            num /= 1024.0
-        return "%.1f%s%s" % (num, 'Yi', suffix)
+# Convert to Human Readable
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
 
 
-    def main(event, context):
-        try:
+def main(event, context):
+    try:
+        for bucket_name in bucket_names:
+            print("Looking into bucket " + str(bucket_name))
+            bucket = s3.Bucket(bucket_name)
+            objs = bucket.objects.filter(Prefix=s3_prefix).all()
+
             backup_success = 0
             for obj in objs:
                 print(obj.last_modified.date(), obj.key, sizeof_fmt(obj.size))
@@ -53,68 +54,68 @@ for bucket_name in bucket_names:
                 notification(file_date=file_date, file_name=file_name, file_size=file_size)
                 print("No backup detected from today: " + str(today))
                 print("--> Last backup file: " + str(file_date), file_name, file_size)
-        except botocore.exceptions.ClientError as e:
-            error_code = e.response['Error']['Code']
-            print(e.response['Error']['Message'])
-            if error_code == '404':
-                print("There is no file in this bucket")
-            else:
-                print(e)
-
-
-    def notification(file_date, file_name, file_size):
-        try:
-            CHARSET = "UTF-8"
-            # Email body for recipients with non-HTML email clients.
-            BODY_TEXT = ("S3 Backup Notifier\r\n"
-                        "Last backup comes from:\r\n"
-                        """str(file_date), file_name, file_size""""\r\n"
-                        "S3 Backup Notifier"
-                        )
-            # HTML body of the email.
-            BODY_HTML = """
-            <html>
-                <body>
-                    <h1>S3 Backup Notifier 👨‍🚒</h1>
-                    <h3>Last backup comes from:</h3>
-                    <table cellpadding="4" cellspacing="4" border="1">
-                    <tr><td>Date</td><td>Name</td><td>Size</td></tr>
-                    <tr><td>""" + str(file_date) + """</td><td>""" + file_name + """</td><td>""" + file_size + """</td></tr>
-                    </table>
-                    <p><a href="https://github.com/z0ph/s3-backup-notifier">S3 Backup Notifier</a></p>
-                </body>
-            </html>
-                        """
-
-            # Provide the contents of the email.
-            response = ses.send_email(
-                Destination={
-                    'ToAddresses': recipients
-                },
-                Message={
-                    'Body': {
-                        'Html': {
-                            'Charset': CHARSET,
-                            'Data': BODY_HTML,
-                        },
-                        'Text': {
-                            'Charset': CHARSET,
-                            'Data': BODY_TEXT,
-                        },
-                    },
-                    'Subject': {
-                        'Charset': CHARSET,
-                        'Data': subject,
-                    },
-                },
-                Source=sender,
-            )
-        # Display an error if something goes wrong.
-        except ClientError as e:
-            print(e.response['Error']['Message'])
+    except botocore.exceptions.ClientError as e:
+        error_code = e.response['Error']['Code']
+        print(e.response['Error']['Message'])
+        if error_code == '404':
+            print("There is no file in this bucket")
         else:
-            print("Email sent! Message ID:"),
-            print(response['MessageId'])
+            print(e)
+
+
+def notification(file_date, file_name, file_size):
+    try:
+        CHARSET = "UTF-8"
+        # Email body for recipients with non-HTML email clients.
+        BODY_TEXT = ("S3 Backup Notifier\r\n"
+                    "Last backup comes from:\r\n"
+                    """str(file_date), file_name, file_size""""\r\n"
+                    "S3 Backup Notifier"
+                    )
+        # HTML body of the email.
+        BODY_HTML = """
+        <html>
+            <body>
+                <h1>S3 Backup Notifier 👨‍🚒</h1>
+                <h3>Last backup comes from:</h3>
+                <table cellpadding="4" cellspacing="4" border="1">
+                <tr><td>Date</td><td>Name</td><td>Size</td></tr>
+                <tr><td>""" + str(file_date) + """</td><td>""" + file_name + """</td><td>""" + file_size + """</td></tr>
+                </table>
+                <p><a href="https://github.com/z0ph/s3-backup-notifier">S3 Backup Notifier</a></p>
+            </body>
+        </html>
+                    """
+
+        # Provide the contents of the email.
+        response = ses.send_email(
+            Destination={
+                'ToAddresses': recipients
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': subject,
+                },
+            },
+            Source=sender,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
 
 # Run locally for testing purpose
 if __name__ == '__main__':
